37b0f31d62a8f22b31a2351913132fed1834ed63
Akhtar Zaman <chanchal@cefalo.com>
2018-07-02 09:19:47 +0600

    renaming bootstrap plugin in name

--- woo-bulk-edit.php
+++ woo-bulk-edit.php
@@ -10,7 +10,7 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Woo_Bulk_Edit
  *
  * @wordpress-plugin
  * Plugin Name:       Woocommerce Easy Product Edit
@@ -35,28 +35,28 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'WOO_BULK_EDIT_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-woo-bulk-edit-activator.php
  */
-function activate_plugin_name() {
+function activate_woo_bulk_edit() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woo-bulk-edit-activator.php';
-	Plugin_Name_Activator::activate();
+	Woo_Bulk_Edit_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-woo-bulk-edit-deactivator.php
  */
-function deactivate_plugin_name() {
+function deactivate_woo_bulk_edit() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woo-bulk-edit-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+	Woo_Bulk_Edit_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook( __FILE__, 'activate_woo_bulk_edit' );
+register_deactivation_hook( __FILE__, 'deactivate_woo_bulk_edit' );
 
 /**
  * The core plugin class that is used to define internationalization,
@@ -73,10 +73,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-woo-bulk-edit.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_woo_bulk_edit() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new Woo_Bulk_Edit();
 	$plugin->run();
 
 }
-run_plugin_name();
+run_woo_bulk_edit();
